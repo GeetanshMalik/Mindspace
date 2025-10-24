64ebc67dd91a8ef5bdef9408385024321cb8efb3
GeetanshMalik <geetanshmalik337@gmail.com>
2025-10-24 09:30:03 +0530

    Update commentService.js

--- frontend/src/services/commentService.js
+++ frontend/src/services/commentService.js
@@ -5,14 +5,9 @@ export const commentService = {
     const response = await api.get(`/comments/${threadId}`);
     return response.data;
   },
-
+  
   createComment: async (threadId, content) => {
-    const response = await api.post(`/comments/${threadId}`, { content });
-    return response.data;
-  },
-
-  deleteComment: async (id) => {
-    const response = await api.delete(`/comments/${id}`);
+    const response = await api.post('/comments', { threadId, content });
     return response.data;
   }
 };
