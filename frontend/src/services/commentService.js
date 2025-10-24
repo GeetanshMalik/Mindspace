33aedd31552e519e22a73ff40295ba57c1ab80ee
GeetanshMalik <geetanshmalik337@gmail.com>
2025-10-24 13:24:04 +0530

    Update commentService.js

--- frontend/src/services/commentService.js
+++ frontend/src/services/commentService.js
@@ -1,13 +1,26 @@
-import api from './api';
+import api from '../config/api';
 
 export const commentService = {
   getComments: async (threadId) => {
-    const response = await api.get(`/comments/${threadId}`);
+    const response = await api.get(`/comments/thread/${threadId}`);
     return response.data;
   },
   
   createComment: async (threadId, content) => {
-    const response = await api.post('/comments', { threadId, content });
+    const response = await api.post('/comments', { 
+      thread: threadId, 
+      content 
+    });
+    return response.data;
+  },
+  
+  likeComment: async (id) => {
+    const response = await api.post(`/comments/${id}/like`);
+    return response.data;
+  },
+  
+  deleteComment: async (id) => {
+    const response = await api.delete(`/comments/${id}`);
     return response.data;
   }
 };
