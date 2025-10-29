7a661e4e4ec4ebc20d2f8328d2ea192714da67fe
GeetanshMalik <geetanshmalik337@gmail.com>
2025-10-30 00:21:03 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const API_URL = process.env.REACT_APP_API_URL || 'https://mindspace-mental-health-forum.onrender.com/api';
-export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'https://mindspace-mental-health-forum.onrender.com';
+export const API_URL ='https://mindspace-mental-health-forum.onrender.com/api';
+export const SOCKET_URL ='https://mindspace-mental-health-forum.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
